67cd687ff1607589a384d145bfe9c2a1b2033318
Tharuka Vishwajith <tharukavishwajiths@gmail.com>
2025-11-21 13:01:38 +0530

    fix issue

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,12 +1,10 @@
 import createImageUrlBuilder from '@sanity/image-url';
 import type { Image } from 'sanity';
-import { apiVersion, dataset, projectId, useCdn } from './env';
+import { dataset, projectId } from './env';
 
 const builder = createImageUrlBuilder({
   projectId,
   dataset,
-  apiVersion,
-  useCdn,
 });
 
 export function urlForImage(source: Image) {
